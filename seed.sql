517fa2dcec69732c555a7d2a1cc35a3dc6310f4f
Raphaelle Lacerda <raphaellelacerda.tds2.senai@gmail.com>
2025-11-13 09:54:00 -0300

    feat: create insert's

--- seed.sql
+++ seed.sql
@@ -29,3 +29,33 @@ CREATE TABLE funcionarios (
     email VARCHAR(100) NOT NULL,
     senha VARCHAR(100) NOT NULL
 );
+
+INSERT
+
+INTO produtos (id_estoque, nome, preco) VALUES
+(1, 'Café com leite', 10.00),
+(2, 'Café com leite e manteiga', 12.00),
+(3, 'Café com leite, manteiga e café de coco', 15.00),
+(4, 'Café com leite, manteiga e café de coco com manteiga', 18.00),
+(5, 'Café com leite, manteiga e café de coco com manteiga e leite', 20.00),
+(6, 'Café com leite, manteiga e café de coco com manteiga e leite e manteiga', 22.00),
+(7, 'Café com leite, manteiga e café de coco com manteiga e leite e manteiga e café', 25.00),
+(8, 'Café com leite, manteiga e café de coco com manteiga e leite e manteiga e café e açúcar', 30.00),
+
+INTO estoque (id_produto, quantidade) VALUES
+(1, 10),
+(2, 5),
+(3, 10),
+(4, 10),
+(5, 10),
+
+INTO vendas (id_funcionario, id_produto, quantidade, preco_total) VALUES
+(1, 1, 2, 20.00),
+(1, 2, 1, 12.00),
+(1, 3, 1, 15.00),
+(1, 4, 1, 18.00),
+
+INTO funcionarios (nome, tipo, email, senha) VALUES
+('Admin', 'administrador', 'admin@email.com', '123456'),
+('João', 'comum', 'joao@email.com', '123456'),
+('Maria', 'comum', 'maria@email.com', '123456'),
